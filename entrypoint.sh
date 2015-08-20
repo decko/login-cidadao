200c18527ef1dbf4778e38bf83a67da22b26e574
gilsondev <me@gilsondev.in>
2015-08-20 12:36:51 -0300

    Tratando entrypoint para desenvolvimento

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-rm -rf /var/www/app/cache/*
-/bin/bash -l -c "$*"
+php app/console cache:clear --no-debug
+php app/console assets:install --symlink web
+# php app/console assetic:dump web
 php app/console doctrine:schema:update --force
+/bin/bash -l -c "$*"
